4fd7cad51bea4b476c60722de2d8a835dabb051c
MistyGH <mforouharartin.2004@gmail.com>
2023-08-25 15:03:34 -0400

    updated resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Resume from '../../assets/Artin\'s_Resume.pdf'
+import Resume from '../../assets/Artins-Resume.pdf'
 
 const CTA = () => {
   return (
     <div className="cta"> 
-        <a href={Resume} download className='btn'>Download Resume</a>
+        <a href={Resume} download="Artin's Resume" className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
